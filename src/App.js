2ff95d32f49dae4157fb77f05e023978911dbc88
Zelipha <zeliphawambui756@gmail.com>
2023-01-17 09:30:08 +0300

    Create Welcome page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import React, { Component } from "react";
+import { ContextProvider } from "./Utils/Context";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WelcomePage from "./Components/WelcomePage/WelcomePage";
 
-function App() {
-  return <div className="App">Hello React!</div>;
+export class App extends Component {
+  render() {
+    return (
+      <ContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+          </Routes>
+        </Router>
+      </ContextProvider>
+    );
+  }
 }
 
 export default App;
