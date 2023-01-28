57daeb0922740f4eced3b9ab156930c955d980d7
Zelipha <zeliphawambui756@gmail.com>
2023-01-28 18:44:32 +0300

    Update the App with Pages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import { ContextProvider } from "./Utils/Context";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WelcomePage from "./Components/WelcomePage/WelcomePage";
+import Navbar from "./Components/Navbar/Navbar";
+import ListingPage from "./Components/ListingPage/ListingPage";
+import DescriptionPage from "./Components/DescriptionPage/DescriptionPage";
+import CartPage from "./Components/CartPage/CartPage";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
 
 export class App extends Component {
   render() {
     return (
       <ContextProvider>
         <Router>
+          <Navbar />
           <Routes>
             <Route path="/" element={<WelcomePage />} />
+            <Route exact path="/404" element={ErrorPage} />
+            <Route exact path="/cart" element={<CartPage />} />
+            <Route exact path="/:category" element={<ListingPage />}></Route>
+            <Route exact path="/:category/:id" element={<DescriptionPage />}></Route>
+            <Route path="*" element={<ErrorPage />} />
           </Routes>
         </Router>
       </ContextProvider>
