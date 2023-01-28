37fc2b9299a906b873239fe5344a00afaa3af7d2
Zelipha <zeliphawambui756@gmail.com>
2023-01-28 18:47:41 +0300

    Create components for the product

---
+++ src/Components/CartPage/CartPage.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Query } from "@apollo/client/react/components";
+import DESCRIPTION from "../../Queries/DescriptionPageQuery";
+import CartProductDetails from "./CartProductDetails";
+import CartProductCounter from "./CartProductCounter";
+import CartProductImages from "./CartProductImages";
+import Context from "../../Utils/Context";
+import "./CartPage.css";
+import { Link } from "react-router-dom";
+
+export class CartPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.cartItem = this.cartItem.bind(this);
+  }
+
+  cartItem = (loading, error, data, item, itemIndex) => {
+    if (loading) return "loading...";
+    if (error) return "error...";
+    if (data) {
+      if (!data.product) {
+        return <Link to="/404" />;
+      }
+      const { gallery, name, brand, prices, attributes } = data.product;
+      return (
+        <div className="cart-product">
+          <CartProductDetails item={item} name={name} brand={brand} prices={prices} attributes={attributes} itemIndex={itemIndex} />
+          <CartProductCounter item={item} itemIndex={itemIndex} />
+          <CartProductImages gallery={gallery} name={name} />
+        </div>
+      );
+    }
+  };
+
+  static contextType = Context;
+  render() {
+    const { addedItems } = this.context;
+    return (
+      <div className="cart-page">
+        <h1 className="cart-page-header">CART</h1>
+        {addedItems.map((item, itemIndex) => {
+          const productId = item[0];
+          return (
+            <Query key={`${productId}:${itemIndex}`} query={DESCRIPTION} variables={{ product_id: productId }}>
+              {({ loading, data, error }) => this.cartItem(loading, error, data, item, itemIndex)}
+            </Query>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default CartPage;
---
+++ src/Components/DescriptionPage/DescriptionPage.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Query } from "@apollo/client/react/components";
+import DESCRIPTION from "../../Queries/DescriptionPageQuery";
+import Images from "./DescriptionImages";
+import Details from "./DescriptionDetails";
+import "./DescriptionPage.css";
+
+export class DescriptionPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.displayProduct = this.displayProduct.bind(this);
+  }
+
+  displayProduct = (loading, error, data) => {
+    const { id } = this.props;
+    if (loading) return "loading...";
+    if (error) return "error...";
+    if (data) {
+      if (!data.product) {
+        return <Link to="/404" />;
+      }
+      const { name, prices, gallery, description, attributes, brand, inStock } = data.product;
+
+      return (
+        <div className="description-page">
+          <Images name={name} gallery={gallery} inStock={inStock} />
+          <Details name={name} brand={brand} prices={prices} attributes={attributes} inStock={inStock} description={description} id={id} />
+        </div>
+      );
+    }
+  };
+
+  render() {
+    const { id } = this.props;
+    return (
+      <Query query={DESCRIPTION} variables={{ product_id: id }}>
+        {({ loading, data, error }) => this.displayProduct(loading, error, data)}
+      </Query>
+    );
+  }
+}
+
+export default DescriptionPage;
